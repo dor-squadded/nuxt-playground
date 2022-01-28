214c8bc8d92852ddec38b8c07a2a0b975715bd5d
dor-kuperman <dorkup@gmail.com>
2022-01-28 18:47:39 +0200

    Fix tests

--- front/layouts/default.spec.js
+++ front/layouts/default.spec.js
@@ -2,8 +2,15 @@ import { shallowMount } from '@vue/test-utils';
 import DefaultLayout from './default.vue';
 
 describe('NuxtLogo', () => {
+	const $route = {
+		name: 'wat',
+	};
 	test('is a Vue instance', () => {
-		const wrapper = shallowMount(DefaultLayout);
+		const wrapper = shallowMount(DefaultLayout, {
+			mocks: {
+				$route,
+			},
+		});
 		expect(wrapper.vm).toBeTruthy();
 	});
 });
